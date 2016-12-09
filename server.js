5b05fc5b175787e97c3d3f184bcf50ee282fbac9
Daniel Pease <daniel.j.pease@gmail.com>
2016-12-09 14:38:57 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict'
+const bodyParser  = require('body-parser');
+const express     = require('express');
+const logger      = require('morgan');
+const app         = express();
+const PORT        = process.argv[2] || process.env.port || 3000;
+
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {console.log('Free hugs at port', PORT)});
